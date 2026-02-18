04f5615e9c6722744e8879a68db13b88ca59923b
Peterdtodd13 <peterdtodd13@gmail.com>
2026-02-18 14:30:47 -0600

    Reorganize imports and clean up code structure

--- src/feature_utils.py
+++ src/feature_utils.py
@@ -21,8 +21,8 @@ def extract_features():
     START_DATE = (datetime.date.today() - datetime.timedelta(days=365)).strftime("%Y-%m-%d")
     END_DATE = datetime.date.today().strftime("%Y-%m-%d")
     stk_tickers = ['AMZN', 'NVDA', 'TSLA']
-ccy_tickers = ['DEXCAUS', 'DEXMXUS']
-idx_tickers = ['SP500', 'DJIA', 'NASDAQCOM']
+    ccy_tickers = ['DEXCAUS', 'DEXMXUS']
+    idx_tickers = ['SP500', 'DJIA', 'NASDAQCOM']
     
     stk_data = yf.download(stk_tickers, start=START_DATE, end=END_DATE, auto_adjust=False)
     #stk_data = web.DataReader(stk_tickers, 'yahoo')
@@ -66,5 +66,6 @@ def get_bitcoin_historical_prices(days = 60):
     df['Date'] = pd.to_datetime(df['Timestamp'], unit='ms').dt.normalize()
     df = df[['Date', 'Close Price (USD)']].set_index('Date')
     return df
+
 
 
